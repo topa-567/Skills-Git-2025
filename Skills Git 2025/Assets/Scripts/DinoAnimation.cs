adeef7690ec1d6be613b21fceefd279bf80d0ec3
topa-567 <saminou.b01@elliotthudsoncollege.ac.uk>
2025-06-12 16:03:35 +0100

    slide 21 Task 2

---
+++ Skills Git 2025/Assets/Scripts/DinoAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimation : MonoBehaviour {
+	Animator MyAnimator;
+	// Use this for initialization
+	void Start () {
+		MyAnimator = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{		
+			MyAnimator.SetBool ("walk", true);
+		} 
+
+		else if (Input.GetKey (KeyCode.DownArrow))
+		{
+			MyAnimator.SetBool ("dead", true);
+		} else if (Input.GetKey (KeyCode.R)) {
+
+			MyAnimator.SetBool ("run", true);
+		}
+		else if (Input.GetKey (KeyCode.Space))
+		{
+			MyAnimator.Play ("Jump");
+		}
+		else if (MyAnimator.GetBool ("dead") == false) 
+		{
+			MyAnimator.SetBool ("idle", true);
+			MyAnimator.SetBool ("walk", false);
+			MyAnimator.SetBool ("run", false);
+		}
+		else if (Input.GetKey (KeyCode.Q) && MyAnimator.GetBool("dead") == true)
+		{
+			MyAnimator.Play("Respawn");
+			MyAnimator.SetBool("dead", false);
+
+		
+
+	}
+	
+}
+}
