eb6d1ebc4a9ef8b76886c5a438f1d5261ae04f53
topa-567 <saminou.b01@elliotthudsoncollege.ac.uk>
2025-06-17 13:57:09 +0100

    Task 3 completed now onto task 4

---
+++ Skills Git 2025/Assets/Scripts/TextController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextController : MonoBehaviour {
+	public TextMeshPro gemCounter;
+
+	GameObject [] totalGems;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		totalGems = GameObject.FindGameObjectsWithTag ("redGem");
+		gemCounter.text = "Gems : " + totalGems.Length.ToString();
+		
+	}
+
+}
