876605bf45b122f0016b9bc43ebfdc9a75ca311a
topa-567 <saminou.b01@elliotthudsoncollege.ac.uk>
2025-06-17 11:31:03 +0100

    end of task 2

---
+++ Skills Git 2025/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 5.1f;
+		rb=GetComponent<Rigidbody2D>();
+
+		
+	}
+		
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.RightArrow))
+			Input.GetKey (KeyCode.LeftArrow);
+		{
+			
+			transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speed, 0, 0);
+		
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+
+			rb.AddForce (Vector2.up * 300);
+		}
+	}
+		
+		void resetPosition()
+		{
+			Debug.Log ("SPIKED RECEIVED");
+			transform.SetPositionAndRotation (new Vector3 (-4.77f, 0.16f, 0), Quaternion.identity);
+		} 	
+
+	}
+	
---
+++ Skills Git 2025/Assets/Scripts/Spikes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject Hero;
+	void Start () {
+		Hero = GameObject.FindGameObjectWithTag ("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("SPIKED!");
+		Hero.SendMessage ("resetPosition");
+	}
+	void resetPosition()
+	{
+		Debug.Log ("SPIKED RECEIVED");
+		transform.SetPositionAndRotation (new Vector3 (-4.77f, 0.16f, 0), Quaternion.identity);
+	} 
+
+}
