90459e5838a8fd0aa933f5b082ef615553e4d819
topa-567 <saminou.b01@elliotthudsoncollege.ac.uk>
2025-06-19 15:20:09 +0100

    Task 5

--- Skills Git 2025/Assets/Scripts/HeroMovement.cs
+++ Skills Git 2025/Assets/Scripts/HeroMovement.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class HeroMovement : MonoBehaviour {
 
 	Rigidbody2D rb;
 	float speed;
+	bool vulnerable;
+	public int lives;
 	// Use this for initialization
+	public void setlives(){
+		lives -= 1;
+		if (lives <= -1)
+		{
+			Debug.Log("End of game");
+			SceneManager.LoadScene ("Lost");
+		}
+	}
 	void Start () {
 		speed = 5.1f;
 		rb=GetComponent<Rigidbody2D>();
-
-		
+		lives = 3;
+		vulnerable = false;
 	}
 		
 	// Update is called once per frame
@@ -34,7 +44,25 @@ public class HeroMovement : MonoBehaviour {
 		{
 			Debug.Log ("SPIKED RECEIVED");
 			transform.SetPositionAndRotation (new Vector3 (-4.77f, 0.16f, 0), Quaternion.identity);
-		} 	
-
+		setlives ();
+		}
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Extra Life") {
+			lives += 1;
+			Destroy (other.gameObject);
+		} else if (other.gameObject.tag == "vulnerable") {
+			Destroy (other.gameObject);
+			vulnerable = true;
+			Debug.Log ("vulnerable = true!");
+			StartCoroutine("VulnerableDeBuff");
+		}
 	}
+			IEnumerator VulnerableDeBuff()
+			{
+		yield return new WaitForSeconds(5f);
+				vulnerable = false;
+				Debug.Log("vulnerable = false!");
+			}
+			}
 	
+
--- Skills Git 2025/Assets/Scripts/TextController.cs
+++ Skills Git 2025/Assets/Scripts/TextController.cs
@@ -5,11 +5,15 @@ using TMPro;
 
 public class TextController : MonoBehaviour {
 	public TextMeshPro gemCounter;
+	public TextMeshPro gameTimer;
+	public TextMeshPro livesCounter;
+	GameObject Hero;
 
+	int lives;
 	GameObject [] totalGems;
 	// Use this for initialization
 	void Start () {
-
+		Hero = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -17,6 +21,9 @@ public class TextController : MonoBehaviour {
 
 		totalGems = GameObject.FindGameObjectsWithTag ("redGem");
 		gemCounter.text = "Gems : " + totalGems.Length.ToString();
+		lives = Hero.GetComponent<HeroMovement>().lives;
+		livesCounter.text = "Lives:" + lives;
+		gameTimer.text = Time.timeSinceLevelLoad.ToString ("0.00");
 		
 	}
 
