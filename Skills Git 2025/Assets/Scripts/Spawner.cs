cde61a929fcaa98d5d3fde24659de821f9e0490e
topa-567 <saminou.b01@elliotthudsoncollege.ac.uk>
2025-06-19 15:47:38 +0100

    task 5 slide 66 question 2

---
+++ Skills Git 2025/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public GameObject altar;
+	bool Spawned;
+	Vector3 SpawnedPosition;
+	// Use this for initialization
+
+	void Start () {
+		Spawned = false;
+		SpawnedPosition = new Vector3 (3.476466f, 5.341311f, 0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void SpawnedAltar() {
+
+	}
+
+}
